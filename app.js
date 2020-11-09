b88f0daec3ba2f07334f78f2ed6e81282e2aebf5
njdeane <njdeane7@gmail.com>
2020-11-09 18:49:55 +1000

    get and post requests update to mailchimp server

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+
 
 const app = express();
 
@@ -12,13 +14,49 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res){
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  console.log(firstName, lastName, email);
-})
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  const url ="https://us2.api.mailchimp.com/3.0/lists/cd0188155d";
+
+  const options = {
+    method: "POST",
+    auth: "Nic:225205e2937518be1c3c621c83455852-us2"
+  }
+
+  const request = https.request(url, options, function(response) {
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+
+});
 
 app.listen(3000, function(req, res) {
   console.log("Server is running on port 3000.");
 });
+
+// API key
+// 225205e2937518be1c3c621c83455852-us2
+
+// list id 
+// cd0188155d
