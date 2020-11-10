97e0f0ab6d1fd39ffc855d9cac956b3e35101da4
njdeane <njdeane7@gmail.com>
2020-11-10 13:43:43 +1000

    final commit

--- app.js
+++ app.js
@@ -3,17 +3,16 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 
-
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
   const firstName = req.body.fName;
   const lastName = req.body.lName;
   const email = req.body.email;
@@ -25,49 +24,47 @@ app.post("/", function(req, res){
         status: "subscribed",
         merge_fields: {
           FNAME: firstName,
-          LNAME: lastName
-        }
-      }
-    ]
+          LNAME: lastName,
+        },
+      },
+    ],
   };
 
   var jsonData = JSON.stringify(data);
 
-  const url ="https://us2.api.mailchimp.com/3.0/lists/cd0188155d";
+  const url = "https://us2.api.mailchimp.com/3.0/lists/cd0188155d";
 
   const options = {
     method: "POST",
-    auth: "Nic:225205e2937518be1c3c621c83455852-us2"
-  }
+    auth: "Nic:225205e2937518be1c3c621c83455852-us2",
+  };
 
-  const request = https.request(url, options, function(response) {
+  const request = https.request(url, options, function (response) {
     if (response.statusCode === 200) {
       res.sendFile(__dirname + "/success.html");
     } else {
       res.sendFile(__dirname + "/failure.html");
     }
 
-
-    response.on("data", function(data){
+    response.on("data", function (data) {
       console.log(JSON.parse(data));
-    })
-  })
+    });
+  });
 
   request.write(jsonData);
   request.end();
-
 });
 
-app.post("/failure", function(req, res){
+app.post("/failure", function (req, res) {
   res.redirect("/");
 });
 
-app.listen(3000, function(req, res) {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on port 3000.");
 });
 
 // API key
 // 225205e2937518be1c3c621c83455852-us2
 
-// list id 
+// list id
 // cd0188155d
